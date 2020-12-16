21b222ee3c80717d1d9485d356d69e8cbf1f91a0
Iranildo Oliveira <48840372+Iranildo10@users.noreply.github.com>
2020-12-15 22:05:17 -0300

    Player, bullet and smoke jetpack

--- Assets/Scripts/PlatformGeneration.cs
+++ Assets/Scripts/PlatformGeneration.cs
@@ -4,34 +4,43 @@ using UnityEngine;
 
 public class PlatformGeneration : MonoBehaviour
 {
-    public GameObject Platform; 
-    public Transform point;
-    public float Distance;
+   public GameObject Platform; //Plataforma
+   public Transform point; // ponto onde acaba a camera
 
-    private float plataforWidth;
+   public float minDistance;
+   public float maxDistance;
+
+   private float platformWidth; // tamanho da plataforma
+   
 
     void Start()
     {
-        if (Platform.GetComponent<BoxCollider2D>())
+        if(Platform.GetComponent<BoxCollider2D>())
         {
-            plataforWidth = Platform.GetComponent<BoxCollider2D>().size.x;
+            //Pegando o tamanho do colisor
+            platformWidth = Platform.GetComponent<BoxCollider2D>().size.x;
         }
         else
         {
-            plataforWidth = Platform.GetComponent<PolygonCollider2D>().bounds.size.x;
-
+            //Pegando tamanho do colisor com formas diferentes
+            platformWidth = Platform.GetComponent<PolygonCollider2D>().bounds.size.x;
         }
     }
 
+   
     void Update()
     {
-        //logica para gerar plataformas 
-        if (transform.position.x < point.position.x)
+        //Logica para gerar plataformas // Se objeto estiver fora da camera
+        if(transform.position.x < point.position.x)
         {
 
-            Distance = Random.Range(3f, 8f);
+            float Distance = Random.Range(minDistance, maxDistance);
+            
+            //Gera nova plataforma
+            transform.position = new Vector3(transform.position.x + platformWidth + Distance, 
+                transform.position.y, 0);
 
-            transform.position = new Vector3(transform.position.x + plataforWidth + Distance, transform.position.y, transform.position.z);
+            //Instaciando nova plataforma na cena
             Instantiate(Platform, transform.position, transform.rotation);
         }
     }
