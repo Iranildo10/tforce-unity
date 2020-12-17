cfa6d32045578b33eb88bc533342e7394138b63c
Iranildo Oliveira <48840372+Iranildo10@users.noreply.github.com>
2020-12-16 22:02:07 -0300

    Spawn Enemies

---
+++ Assets/Scripts/SpawnEnemyns.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemyns : MonoBehaviour
+{
+
+    public float initialTime;
+
+    public float minTime;
+
+    public float maxTime;
+
+    public List<GameObject> EnemiesList = new List<GameObject>();
+
+    public float timeControl;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeControl += Time.deltaTime;
+
+        if(timeControl >= initialTime){
+             Instantiate(EnemiesList[Random.Range(0, EnemiesList.Count)], 
+                transform.position + new Vector3(0, Random.Range(-2,3), 0), transform.rotation);
+
+            initialTime = Random.Range(minTime, maxTime);
+            timeControl = 0;
+        }
+    }
+}
